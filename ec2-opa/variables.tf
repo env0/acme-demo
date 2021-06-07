723653419e564850821b0e5759b7bec3b543a549
Andrew Way <andrew.way@env0.com>
2021-06-06 19:29:17 -0700

    opa example

---
+++ ec2-opa/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  type    = string
+  default = "env0-acme-ec2"
+}
+
+variable "instance_count" {
+  type    = number
+  default = 1
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3a.micro"
+}
