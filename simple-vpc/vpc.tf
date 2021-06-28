9b7c7d6f91004cef7d625f39bd04fcd38b81febc
Andrew Way <andrew.way@env0.com>
2021-06-27 22:13:21 -0700

    simple vpc

---
+++ simple-vpc/vpc.tf
@@ -0,0 +1,12 @@
+module "acme-vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.1.0"
+
+  name = var.name
+  cidr = var.cidr
+
+  enable_nat_gateway = var.enable_nat_gateway
+
+  azs             = ["${var.region}a", "${var.region}b"]
+  private_subnets = var.private_subnets
+}
