13fba1e44c034f170eae4e0aa85284090d0e6262
Andrew Way <andrew.way@env0.com>
2021-09-22 14:28:52 -0700

    simple examle that does not require cloud credentials

---
+++ simple-terraform/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.1.0"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "2.1.0"
+    }
+  }
+}
+
+provider "local" {
+  # Configuration options
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "local_file" "randomstring" {
+    content     = random_string.random.result
+    filename = "${path.module}/randomstring.txt"
+}
+
+variable "refresh_date" {
+  type = string
+  default = "00/00/0000"
+}
+
+resource "random_string" "random" {
+  keepers = {
+      refresh_date = var.refresh_date
+  }
+  length           = 5
+  upper            = true
+  special          = false
+}
+
+output "random_string" {
+    value = random_string.random.result
+}
