12554849cb034c08446c186956f337c71f1b8e1a
Andrew Way <andrew.way@env0.com>
2021-09-24 16:52:46 -0700

    fix userdata

--- ansible/control/ec2.tf
+++ ansible/control/ec2.tf
@@ -18,8 +18,8 @@ module "ansible-control" {
 sudo apt-get -y update
 sudo apt-get -y install software-properties-common 
 sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install ansible
+sudo apt-get -y update
+sudo apt-get -y install ansible
 EOF
 
   tags = {
