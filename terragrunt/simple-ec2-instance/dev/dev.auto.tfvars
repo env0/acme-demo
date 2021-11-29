d9d848f8bb5522540a522ac3374d8617dd40d7f7
Andrew Way <andrew.way@env0.com>
2021-11-28 19:49:19 -0800

    new tg example

---
+++ terragrunt/simple-ec2-instance/dev/dev.auto.tfvars
@@ -0,0 +1,2 @@
+instance_count = 1
+instance_type = "t3a.micro"
--- terragrunt/simple-ec2-instance/provider.tf
+++ terragrunt/simple-ec2-instance/provider.tf
@@ -1,11 +1,11 @@
 terraform {
-  required_version = "~>0.15.4"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "3.41.0"
     }
   }
+  backend "s3" {}
 }
 
 
