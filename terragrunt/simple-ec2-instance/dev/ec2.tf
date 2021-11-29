a68d0957fe763b968a912aae23876f68410d40dc
Andrew Way <andrew.way@env0.com>
2021-11-28 20:15:33 -0800

    fmt

--- terragrunt/simple-ec2-instance/dev/ec2.tf
+++ terragrunt/simple-ec2-instance/dev/ec2.tf
@@ -25,26 +25,26 @@ module "acme-ec2" {
   name           = var.name
   instance_count = var.instance_count
 
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = var.instance_type
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
   #key_name               = "AWay"
   #vpc_security_group_ids = [data.aws_security_group.web_server.id]
-  subnet_ids             = data.aws_subnet_ids.selected.ids
+  subnet_ids = data.aws_subnet_ids.selected.ids
 
   associate_public_ip_address = false
 
-#   user_data = <<EOF
-# #!/bin/bash
-# sudo apt-get -y update
-# sudo apt-get -y install nginx
-# export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
-# sudo echo "Welcome to env0, this is: $PUBLIC_IPV4" > /usr/share/nginx/html/index.html
-# EOF
+  #   user_data = <<EOF
+  # #!/bin/bash
+  # sudo apt-get -y update
+  # sudo apt-get -y install nginx
+  # export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+  # sudo echo "Welcome to env0, this is: $PUBLIC_IPV4" > /usr/share/nginx/html/index.html
+  # EOF
 
   tags = {
-    Terraform   = "true"
-    Owner       = "acme demo org"
-    Terragrunt  = "true"
+    Terraform  = "true"
+    Owner      = "acme demo org"
+    Terragrunt = "true"
   }
 }
 
