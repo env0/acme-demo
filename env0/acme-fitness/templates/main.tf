feefeb66ddbfbdcb8cd5f748dd3580af60d50e6e
Andrew Way <andrew.way@env0.com>
2021-12-14 13:39:01 -0800

    add terraform version

--- env0/acme-fitness/templates/main.tf
+++ env0/acme-fitness/templates/main.tf
@@ -11,8 +11,9 @@ provider "env0" {
 }
 
 module "template" {
-  source = "api.env0.com/c89e9161-8350-44a5-81cf-9eb26605e195/template/env0"
-  version = "~> 0.0.1"
+#   source = "api.env0.com/c89e9161-8350-44a5-81cf-9eb26605e195/template/env0"
+#   version = "~> 0.0.1"
+  source = "git@github.com:env0/terraform-env0-template.git"
 
   templates = var.templates
 }
@@ -25,7 +26,8 @@ variable "templates" {
       repository             = string
       path                   = string
       revision               = string
-      ssh_keys               = list(string)
+      terraform_version      = string
+      ssh_keys               = list(map(string))
       github_installation_id = number
       projects               = list(string)
     })
