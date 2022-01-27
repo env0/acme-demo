698891f88bf29192f2b90a3196ad2e2738283a85
Andrew Way <andrew.way@env0.com>
2022-01-27 15:53:05 -0800

    update provider

--- ec2-checkov/provider.tf
+++ ec2-checkov/provider.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~>0.15.4"
+  required_version = "~>1.1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.41.0"
+      version = "~>3.41.0"
     }
   }
 }
