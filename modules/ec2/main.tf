4014e48dfa1340b067f825d1d77983ad034e65b8
Andrew Way <andrew.way@env0.com>
2022-02-10 15:54:20 -0800

    add ec2 module

---
+++ modules/ec2/main.tf
@@ -0,0 +1,65 @@
+module "acme-ec2" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 2.0"
+
+  name           = var.name
+  instance_count = var.instance_count
+
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+
+  #key_name               = "AWay"
+  #vpc_security_group_ids = [data.aws_security_group.web_server.id]
+  subnet_ids = data.aws_subnet_ids.selected.ids
+
+  associate_public_ip_address = true
+
+  #   user_data = <<EOF
+  # #!/bin/bash
+  # sudo apt-get -y update
+  # sudo apt-get -y install nginx
+  # export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+  # sudo echo "Welcome to env0, this is: $PUBLIC_IPV4" > /usr/share/nginx/html/index.html
+  # EOF
+
+  tags = var.tags
+}
+
+# data "aws_security_group" "web_server" {
+#   name = "webserver"
+# }
+
+resource "aws_volume_attachment" "volume_attachment" {
+  count = var.instance_count
+
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.ebs[count.index].id
+  instance_id = module.acme-ec2.id[count.index]
+}
+
+resource "aws_ebs_volume" "ebs" {
+  count = var.instance_count
+
+  availability_zone = module.acme-ec2.availability_zone[count.index]
+  size              = var.ebs_size
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
+data "aws_vpc" "selected" {
+  id = var.vpc_id
+}
+
+data "aws_subnet_ids" "selected" {
+  vpc_id = data.aws_vpc.selected.id
+}
---
+++ modules/ec2/variables.tf
@@ -0,0 +1,27 @@
+variable "name" {
+  type    = string
+  default = "env0-acme-ec2"
+}
+
+variable "instance_count" {
+  type    = number
+  default = 2
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3a.large"
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "tags" {
+  type = map
+}
+
+variable "ebs_size" {
+  type    = number
+  default = 1
+}
