4d474a0f0911d9bac9c13f5b4d5cdc94cc1a8ab3
Andrew Way <andrew.way@env0.com>
2022-03-14 13:40:41 -0700

    whole deployment

---
+++ complex-deployment/infra-whole/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    env0 = {
+      source = "env0/env0"
+      version = ">= 0.2.28"
+    }
+  }
+}
+
+variable "project" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+data "env0_project" "project" {
+  name = var.project
+}
+
+resource "env0_environment" "infra-base" {
+  name        = "infra-base-${var.name}"
+  project_id  = data.env0_project.project.id
+  template_id = "84727784-587b-4e58-83ea-b0e2f9c872bd"
+  approve_plan_automatically = true
+}
+
+resource "env0_environment" "infra-mid" {
+  name        = "infra-mid-${var.name}"
+  project_id  = data.env0_project.project.id
+  template_id = "8c86ee3e-c42d-4f7b-b7be-8513f20fcc65"
+  approve_plan_automatically = true
+}
+
+resource "env0_environment" "infra-top" {
+  name        = "infra-top-${var.name}"
+  project_id  = data.env0_project.project.id
+  template_id = "5cfcb877-bdf2-402f-a5fc-343d2e1a8f8e"
+  approve_plan_automatically = true
+}
+
+resource "env0_workflow_triggers" "base-mid" {
+  environment_id = env0_environment.infra-base.id
+  downstream_environment_ids = [
+    env0_environment.infra-mid.id
+  ]
+}
+
+resource "env0_workflow_triggers" "mid-top" {
+  environment_id = env0_environment.infra-mid.id
+  downstream_environment_ids = [
+    env0_environment.infra-top.id
+  ]
+}
