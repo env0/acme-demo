661ea0b06cdeb7275589f8e17d8dc0c5f3eb2ca4
Andrew Way <andrew.way@env0.com>
2022-03-25 06:56:29 -0700

    Update ec2.tf
    clean up

--- simple-ec2-instance/ec2.tf
+++ simple-ec2-instance/ec2.tf
@@ -7,20 +7,9 @@ module "acme-ec2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
-
-  #key_name               = "AWay"
-  #vpc_security_group_ids = [data.aws_security_group.web_server.id]
   subnet_ids = data.aws_subnet_ids.selected.ids
 
   associate_public_ip_address = true
-
-  #   user_data = <<EOF
-  # #!/bin/bash
-  # sudo apt-get -y update
-  # sudo apt-get -y install nginx
-  # export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
-  # sudo echo "Welcome to env0, this is: $PUBLIC_IPV4" > /usr/share/nginx/html/index.html
-  # EOF
 
   tags = {
     Terraform   = "true"
@@ -28,22 +17,6 @@ module "acme-ec2" {
     Test        = "new tag"
     Demo        = "March 15 12:34PM PT"
   }
-}
-
-# data "aws_security_group" "web_server" {
-#   name = "webserver"
-# }
-
-resource "aws_volume_attachment" "volume_attachment" {
-
-  device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.ebs.id
-  instance_id = module.acme-ec2.id
-}
-
-resource "aws_ebs_volume" "ebs" {
-  availability_zone = module.acme-ec2.availability_zone[1]
-  size              = var.ebs_size
 }
 
 data "aws_ami" "ubuntu" {
