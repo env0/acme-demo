a11673c6a1bbe28cd74b5eea0bf0d6b7ce633641
Andrew Way <andrew.way@env0.com>
2022-05-05 09:44:53 -0700

    clean up

--- simple-terraform/main.tf
+++ simple-terraform/main.tf
@@ -4,21 +4,8 @@ terraform {
       source = "hashicorp/random"
       version = "3.1.0"
     }
-    local = {
-      source = "hashicorp/local"
-      version = "2.1.0"
-    }
   }
 }
-
-provider "random" {
-  # Configuration options
-}
-
-# resource "local_file" "randomstring" {
-#     content     = random_string.random.result
-#     filename = "/tmp/49f45071-9ea0-41fb-a36b-768f703a79c9/randomstring.txt"
-# }
 
 variable "refresh_date" {
   type = string
@@ -36,8 +23,4 @@ resource "random_string" "random" {
 
 output "random_string" {
     value = random_string.random.result
-}
-
-output "environment_variables" {
-  value = fileexists("env0.system-env-vars.json") ? jsondecode(file("env0.system-env-vars.json")) : jsondecode ("{\"result\"}:{\"env0.system-env-vars.json not found\"}")
 }
