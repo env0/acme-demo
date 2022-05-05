3db4ccee8fc6b5f7980c3862e6fc2737c86c0c64
Andrew Way <andrew.way@env0.com>
2022-05-05 14:28:24 -0700

    add role_arn to backend config

--- simple-static-website-s3/provider.tf
+++ simple-static-website-s3/provider.tf
@@ -6,6 +6,14 @@ terraform {
       version = "3.41.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "env0-acme-tfstate"
+    dynamodb_table = "env0-acme-tfstate-lock"
+    key            = "acme-demo-s3"
+    region         = "us-west-2"
+    role_arn       = "arn:aws:iam::326535729404:role/env0-acme-assume-role"
+  }
 }
 
 provider "aws" {
