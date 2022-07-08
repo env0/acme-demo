31f42ee0a1e87fc11971444be65c4d68518e9844
Andrew Way <andrew.way@env0.com>
2022-07-08 08:51:04 -0700

    Update ec2.tf

--- simple-ec2-instance/ec2.tf
+++ simple-ec2-instance/ec2.tf
@@ -15,7 +15,7 @@ module "acme-ec2" {
     Terraform   = "true"
     Owner       = "acme demo org"
     Test        = "new tag"
-    Demo        = "July 08"
+    Demo        = "July 08 8:50AM"
   }
 }
 
