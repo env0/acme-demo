30ee1504928822f35b1f73a692e8ad4d684bac8d
Andrew Way <andrew.way@env0.com>
2022-07-12 09:57:28 -0700

    agent script updates

--- agent-server/scripts/agentInstall.sh
+++ agent-server/scripts/agentInstall.sh
@@ -1,15 +1,15 @@
 #!/bin/bash 
 
+source setup.sh
+
+sudo chmod 666 $KUBECONFIG
+
+if [[ (jq --version) -ne 0 ]]; then
+  echo "jq not found, installing jq"
+  sudo apt-get install jq -y
+fi
+
 ## need JQ 
-ENV0_API_KEY=
-ENV0_API_SECRET=
-ENV0_ORGANIZATION_ID=
-
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-
-#source setup.sh
->>>>>>> de40eaf (agent install update)
-
 ## curl for list of agents, take the first one (default) 
 ENV0_AGENT_KEY=$(curl -u $ENV0_API_KEY:$ENV0_API_SECRET https://api.env0.com/agents?organizationId=$ENV0_ORGANIZATION_ID | jq -r .[0].agentKey)
 
@@ -20,7 +20,7 @@ curl -u $ENV0_API_KEY:$ENV0_API_SECRET https://api.env0.com/agents/$ENV0_AGENT_K
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | sudo bash
 helm repo add env0 https://env0.github.io/self-hosted
 helm repo update
-helm install --create-namespace env0-agent env0/env0-agent --namespace env0-agent -f values.yaml --set storageClassName=local-path --kubeconfig=/etc/rancher/k3s/k3s.yaml
+helm install --create-namespace env0-agent env0/env0-agent --namespace env0-agent -f ~/values.yaml -f ~/customer-values.yaml --set storageClassName=local-path --kubeconfig=/etc/rancher/k3s/k3s.yaml
 
 kubectl delete pvc env0-state-volume-claim -n env0-agent
 kubectl apply -f manifests/pvc.yaml
---
+++ agent-server/scripts/prereq.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# install k3s
+
+
+# install helm
+
+
+# add .bashrc helper stuff (kubectl auto-completion etc)
