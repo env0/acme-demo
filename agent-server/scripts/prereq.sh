203b9a05635968998ccf001b15a246f59b17a706
Andrew Way <andrew.way@env0.com>
2022-07-12 14:30:50 -0700

    add downloads to install

--- agent-server/scripts/prereq.sh
+++ agent-server/scripts/prereq.sh
@@ -12,3 +12,9 @@ chmod 700 get_helm.sh
 echo 'source <(kubectl completion bash)' >>~/.bashrc
 echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
+
+# download relevant files
+curl -sfLO https://raw.githubusercontent.com/env0/acme-demo/main/agent-server/scripts/agentInstall.sh
+curl -sfLO https://raw.githubusercontent.com/env0/acme-demo/main/agent-server/scripts/agentUpdate.sh
+curl -sfLO https://raw.githubusercontent.com/env0/acme-demo/main/agent-server/scripts/setup.sh
+curl -sfL -o manifests/pvc.yaml https://raw.githubusercontent.com/env0/acme-demo/main/agent-server/scripts/manifests/pvc.yaml
