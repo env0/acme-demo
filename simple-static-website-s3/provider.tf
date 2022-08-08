29401a3d11d388f820d39cb38d25ee13e8d224d5
Andrew Way <andrew.way@env0.com>
2022-08-08 13:27:40 -0700

    Update provider.tf

--- simple-static-website-s3/provider.tf
+++ simple-static-website-s3/provider.tf
@@ -11,8 +11,9 @@ terraform {
     dynamodb_table = "env0-acme-tfstate-lock"
     key            = "acme-demo-s3"
     region         = "us-west-2"
-    role_arn       = "arn:aws:iam::326535729404:role/env0-acme-assume-role"
-    //external_id    = "value"  # use ENV0_TERRAFORM_BACKEND_CONFIG=external_id=[external_id_value]
+    # the role is being assumed from the deployer role - so you will need to configure assume role chaining
+    role_arn       = "arn:aws:iam::326535729404:role/env0-acme-assume-role" 
+    # external_id    = "value"  # (optional) use ENV0_TERRAFORM_BACKEND_CONFIG=external_id=[external_id_value]
   }
 }
 
