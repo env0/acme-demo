634a89631b82ca1fc209263d0c3ba7b6cf5a9850
Andrew Way <andrew.way@env0.com>
2022-08-17 15:15:41 -0700

    Update cleanup-org.sh
    bump year

--- scripts/cleanup-org.sh
+++ scripts/cleanup-org.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ################################################################################
-# Copyright 2021 EnvZero, Inc.
+# Copyright 2022 EnvZero, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
