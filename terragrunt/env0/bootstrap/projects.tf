eee4e8c9473eab86100e3bc7c3c1a8027f1fec95
Andrew Way <andrew.way@env0.com>
2022-08-29 20:23:34 -0700

    Update projects.tf

--- terragrunt/env0/bootstrap/projects.tf
+++ terragrunt/env0/bootstrap/projects.tf
@@ -2,8 +2,8 @@ variable "projects" {
   type = list(string)
 }
 
-# should really only be run once, does not handle recreation
 resource "env0_project" "this" {
   for_each = toset(var.projects)
   name     = each.value
+  # TODO ADD CREDENTIALS
 }
