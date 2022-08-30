361cf1339e2e7fe0dc3a81573fb439b478c3f7a5
Andrew Way <andrew.way@env0.com>
2022-08-29 20:25:49 -0700

    Update projects.tf

--- terragrunt/env0/bootstrap/projects.tf
+++ terragrunt/env0/bootstrap/projects.tf
@@ -5,5 +5,7 @@ variable "projects" {
 resource "env0_project" "this" {
   for_each = toset(var.projects)
   name     = each.value
-  # TODO ADD CREDENTIALS
 }
+
+# TODO ADD CREDENTIALS
+# https://registry.terraform.io/providers/env0/env0/latest/docs/resources/cloud_credentials_project_assignment
