2853095421a0d98dac74d9be9cb0817e62a99a30
Andrew Way <andrew.way@env0.com>
2022-09-29 14:18:10 -0700

    use ARM environment variables

--- azure/oidc/oidc_login.sh
+++ azure/oidc/oidc_login.sh
@@ -3,22 +3,22 @@
 # Calls Microsoft OAUTH2 endpoint to retrieve token 
 # Must configure federated identity client: see https://docs.env0.com/docs/oidc-integrations for more information
 
-#$TENANT_ID 
-#$CLIENT_ID 
+#$ARM_TENANT_ID 
+#$ARM_CLIENT_ID 
 
-if [[ -z $TENANT_ID ]]; then 
+if [[ -z $ARM_TENANT_ID ]]; then 
   echo "Please specify TENANT_ID"
   exit 1
 fi
 
-if [[ -z $CLIENT_ID ]]; then 
+if [[ -z $ARM_CLIENT_ID ]]; then 
   echo "Please specify CLIENT_ID"
   exit 1
 fi 
 
-ACCESS_TOKEN=$(curl -L -X POST "https://login.microsoftonline.com/$TENANT_ID/oauth2/v2.0/token" \
+ACCESS_TOKEN=$(curl -L -X POST "https://login.microsoftonline.com/$ARM_TENANT_ID/oauth2/v2.0/token" \
 -H "Content-Type: application/x-www-form-urlencoded" \
---data-urlencode "client_id=$CLIENT_ID" \
+--data-urlencode "client_id=$ARM_CLIENT_ID" \
 --data-urlencode "client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer" \
 --data-urlencode "client_assertion=$ENV0_OIDC_TOKEN" \
 --data-urlencode "grant_type=client_credentials" \
