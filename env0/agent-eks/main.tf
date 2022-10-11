a7572ace5bb009bb577341689cd697dc7c6a1fe6
Andrew Way <andrew.way@env0.com>
2022-10-11 15:56:28 -0700

    add load_config_file = false

--- env0/agent-eks/main.tf
+++ env0/agent-eks/main.tf
@@ -29,6 +29,7 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
 }
 
 provider "kubectl" {
@@ -47,6 +48,7 @@ provider "helm" {
       args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
       command     = "aws"
     }
+    load_config_file       = false
   }
 }
 
