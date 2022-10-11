761603d043c85e6e7e38dbbac974ea2a04bf7d03
Andrew Way <andrew.way@env0.com>
2022-10-11 15:29:20 -0700

    use aws-iam-authenticator

--- env0/agent-eks/main.tf
+++ env0/agent-eks/main.tf
@@ -34,7 +34,14 @@ provider "kubernetes" {
 provider "kubectl" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  # token                  = data.aws_eks_cluster_auth.cluster.token
+  
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws-iam-authenticator"
+    args = ["token", "-i", data.aws_eks_cluster.cluster.name]
+  }
+  
   load_config_file       = false
 }
 
