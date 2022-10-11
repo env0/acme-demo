7dd4de45dfd91d906c8464129b399b2a620e65c5
Andrew Way <andrew.way@env0.com>
2022-10-11 16:23:55 -0700

    modify provider versions

--- env0/agent-eks/main.tf
+++ env0/agent-eks/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
     aws = {
       source = "hashicorp/aws"
-      version = "~>3.68.0"
+      version = "~>3.75.0"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
