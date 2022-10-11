b80778a2588a9ce41828b37e307c3e2ffd7f4be6
Andrew Way <andrew.way@env0.com>
2022-10-11 15:39:10 -0700

    revert

--- env0/agent-eks/main.tf
+++ env0/agent-eks/main.tf
@@ -34,14 +34,7 @@ provider "kubernetes" {
 provider "kubectl" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  # token                  = data.aws_eks_cluster_auth.cluster.token
-  
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "aws"
-    args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
-  }
-
+  token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
 }
 
