7f7cff61f91c92ef8f8ef49ddb43890b8025b867
Andrew Way <andrew.way@env0.com>
2022-10-11 10:25:52 -0700

    agent

--- env0/agent-eks/main.tf
+++ env0/agent-eks/main.tf
@@ -1,8 +1,8 @@
 module "env0-agent-eks" {
   source = "git@github.com:env0/k8s-modules.git//aws"
-
   region = var.region
-} 
+  cluster_name = var.cluster-name
+}
 
 variable "region" {
   type = string
@@ -12,4 +12,42 @@ variable "region" {
 variable "cluster-name" {
   type = string
   default = "env0 agent eks"
+}
+
+// providers.tf
+provider "aws" {
+  region = var.region
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.env0-agent-eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.env0-agent-eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
+  }
 }
