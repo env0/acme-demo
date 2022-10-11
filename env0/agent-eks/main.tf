1bbbb2422a45c687fbd5856bec50a3133628b489
Andrew Way <andrew.way@env0.com>
2022-10-11 16:04:48 -0700

    try bumping k8s provider version

--- env0/agent-eks/main.tf
+++ env0/agent-eks/main.tf
@@ -26,14 +26,15 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "kubernetes" {
+  version                = ">=2.14.0"
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  #token                  = data.aws_eks_cluster_auth.cluster.token
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
-    command     = "aws"
-  }
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  # exec {
+  #   api_version = "client.authentication.k8s.io/v1alpha1"
+  #   args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+  #   command     = "aws"
+  # }
 }
 
 provider "kubectl" {
