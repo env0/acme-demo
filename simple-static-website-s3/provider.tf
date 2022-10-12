99fddc6ed719fa4077bdd1ecbed1fcb92c2d3b28
Andrew Way <andrew.way@env0.com>
2022-10-12 12:52:03 -0700

    scenario 1 - no backend defined

--- simple-static-website-s3/provider.tf
+++ simple-static-website-s3/provider.tf
@@ -6,15 +6,7 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "env0-acme-tfstate"
-    dynamodb_table = "env0-acme-tfstate-lock"
-    key            = "acme-demo-s3"
-    region         = "us-west-2"
-    # the role is being assumed from the deployer role - so you will need to configure assume role chaining
-    role_arn       = "arn:aws:iam::326535729404:role/env0-acme-assume-role" 
-    # external_id    = "value"  # (optional) use ENV0_TERRAFORM_BACKEND_CONFIG=external_id=[external_id_value]
-  }
+  # scenario 1 no backend defined. 
 }
 
 provider "aws" {
