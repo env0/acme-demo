9aba0e5035caf7187bb9d48ed9242723239fc252
Andrew Way <andrew.way@env0.com>
2022-10-11 21:51:25 -0700

    remove config_path - doesnt do anything

--- env0/agent-eks/main.tf
+++ env0/agent-eks/main.tf
@@ -37,7 +37,6 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  config_path            = "~/.kube/config"
   # exec {
   #   api_version = "client.authentication.k8s.io/v1alpha1"
   #   args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
