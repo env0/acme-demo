159341c17b539e45119d3147ea4c5fa5dde6c473
Andrew Way <andrew.way@env0.com>
2022-10-12 13:06:02 -0700

    add backend and remove env0.yaml

--- simple-static-website-s3/provider.tf
+++ simple-static-website-s3/provider.tf
@@ -6,7 +6,15 @@ terraform {
     }
   }
 
-  # scenario 1 no backend defined. 
+  # scenario 1b use env0's remote backend. 
+    backend "remote" {
+    hostname = "backend.api.env0.com"
+    organization = "bde19c6d-d0dc-4b11-a951-8f43fe49db92.d80c9a87-2794-4f04-94fd-3fc675444072"
+
+    workspaces {
+      name = "static-website-dev1"
+    }
+  }
 }
 
 provider "aws" {
