3d22106516e225376a47c6fecd8261de8cf26f0a
Andrew Way <andrew.way@env0.com>
2022-10-13 16:06:22 -0700

    add ability to not output kubeconfig

--- env0/agent-eks/eks/main.tf
+++ env0/agent-eks/eks/main.tf
@@ -5,16 +5,11 @@ data "aws_vpc" "this" {
 }
 
 module "eks" {
-  source        = "git@github.com:env0/k8s-modules.git//aws/eks"
-  vpc_id        = data.aws_vpc.this.id
-  cluster_name  = var.cluster_name
-  map_roles     = var.map_roles
-  min_capacity  = var.min_capacity
-  instance_type = var.instance_type
-
-  lifecycle {
-    ignore_changes = [
-      module.eks.module.eks.local_file.kubeconfig[0]
-    ]
-  }
+  source           = "git@github.com:env0/k8s-modules.git//aws/eks?ref=feat/kubeconfig-option"
+  vpc_id           = data.aws_vpc.this.id
+  cluster_name     = var.cluster_name
+  map_roles        = var.map_roles
+  min_capacity     = var.min_capacity
+  instance_type    = var.instance_type
+  write_kubeconfig = false
 }
