7bc7b1540da0acc4242dc4799b48517a4e8a9f8a
Andrew Way <andrew.way@env0.com>
2022-11-01 10:44:37 -0700

    true blue/green

--- env0/agent-eks/eks/main.tf
+++ env0/agent-eks/eks/main.tf
@@ -5,11 +5,13 @@ data "aws_vpc" "this" {
 }
 
 module "eks" {
-  source           = "git@github.com:env0/k8s-modules.git//aws/eks?ref=feat/kubeconfig-option"
+  source           = "git@github.com:env0/k8s-modules.git//aws/eks?ref=eks-node-groups"
   vpc_id           = data.aws_vpc.this.id
   cluster_name     = var.cluster_name
   map_roles        = var.map_roles
   min_capacity     = var.min_capacity
   instance_type    = var.instance_type
-  write_kubeconfig = false
+  #write_kubeconfig = false
+  blue_instance_type  = "t3a.2xlarge"
+  green_instance_type = ""
 }
