14e27d93f33c9353d62ff33995782f42fd0a884f
Kevin Damaso <kevindamaso@gmail.com>
2022-11-08 13:10:15 -0800

    update backend on provider.tf

--- simple-static-website-s3/provider.tf
+++ simple-static-website-s3/provider.tf
@@ -9,7 +9,7 @@ terraform {
   # scenario 1b use env0's remote backend. 
   backend "remote" {
     hostname = "backend.api.env0.com"
-    organization = "bde19c6d-d0dc-4b11-a951-8f43fe49db92.d80c9a87-2794-4f04-94fd-3fc675444072"
+    organization = "bde19c6d-d0dc-4b11-a951-8f43fe49db92.d80c9a87-2794-4f04-94fd-3fc675444073"
 
     workspaces {
       name = "static-website-dev1"
