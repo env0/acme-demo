31cf1b4b23c3914041881361251e31ff5c242d65
Kevin Damaso <kevindamaso@gmail.com>
2022-11-18 14:13:32 -0800

    change it back to west-2

--- simple-static-website-s3/provider.tf
+++ simple-static-website-s3/provider.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "env0-acme-tfstate"
     dynamodb_table = "env0-acme-tfstate-lock"
     key            = "acme-demo-s3"
-    region         = "us-east-2"
+    region         = "us-west-2"
     # the role is being assumed from the deployer role - so you will need to configure assume role chaining
     role_arn       = "arn:aws:iam::326535729404:role/env0-acme-assume-role" 
     # external_id    = "value"  # (optional) use ENV0_TERRAFORM_BACKEND_CONFIG=external_id=[external_id_value]
@@ -18,5 +18,5 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "us-west-2"
 }
