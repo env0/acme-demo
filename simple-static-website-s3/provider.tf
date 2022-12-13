468ef4bd28f2decc90e8de21536d8d907043cc55
Andrew Way <away@env0.com>
2022-12-13 14:43:58 -0800

    remove backend

--- simple-static-website-s3/provider.tf
+++ simple-static-website-s3/provider.tf
@@ -5,16 +5,6 @@ terraform {
       version = "4.46.0"
     }
   }
-
-  backend "s3" {
-    bucket         = "env0-acme-tfstate"
-    dynamodb_table = "env0-acme-tfstate-lock"
-    key            = "acme-demo-s3"
-    region         = "us-west-2"
-    # the role is being assumed from the deployer role - so you will need to configure assume role chaining
-    role_arn       = "arn:aws:iam::326535729404:role/env0-acme-assume-role" 
-    # external_id    = "value"  # (optional) use ENV0_TERRAFORM_BACKEND_CONFIG=external_id=[external_id_value]
-  }
 }
 
 provider "aws" {
