b67d93fd808f70b9d6b60fe6e1fe8fdafc530221
Andrew Way <away@env0.com>
2023-01-25 17:14:23 -0800

    add cdktf s3 example

---
+++ cdktf/s3/main.ts
@@ -0,0 +1,25 @@
+// Copyright (c) HashiCorp, Inc
+// SPDX-License-Identifier: MPL-2.0
+import { Construct } from "constructs";
+import { App, TerraformStack } from "cdktf";
+import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
+import { S3Bucket } from "@cdktf/provider-aws/lib/s3-bucket";
+
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    // define resources here
+    new AwsProvider(this, "AWS", {
+      region: "us-west-1",
+    });
+
+    new S3Bucket(this, "mybucket", {
+      bucket: "cdktf-demo",
+    });
+  }
+}
+
+const app = new App();
+new MyStack(app, "s3");
+app.synth();
