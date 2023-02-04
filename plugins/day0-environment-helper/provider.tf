5970f2ace21babf4b38f4449814235e97ca29d3b
Andrew Way <away@env0.com>
2023-02-03 16:43:16 -0800

    add more scaffolding

---
+++ plugins/day0-environment-helper/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    env0 = {
+      source  = "env0/env0"
+      version = ">=1.3.5"
+    }
+  }
+
+  backend "remote" {
+    hostname = "backend.api.env0.com"
+    organization = "bde19c6d-d0dc-4b11-a951-8f43fe49db92"
+    workspaces {
+      name = "day0-environment-2023-02"
+    }
+  }
+}
+
+provider "env0" {
+}
