03c946c507e64c9982c0dab36d8e3d5696408047
Andrew Way <away@env0.com>
2023-02-04 10:10:51 -0800

    test tfprovider

--- plugins/day0-environment-helper/provider.tf
+++ plugins/day0-environment-helper/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "remote" {
-    hostname = "backend.api.env0.com"
+    hostname     = "backend.api.env0.com"
     organization = "bde19c6d-d0dc-4b11-a951-8f43fe49db92"
     workspaces {
       name = "day0-environment-2023-02"
--- plugins/day0-environment-helper/variables.tf
+++ plugins/day0-environment-helper/variables.tf
@@ -2,3 +2,21 @@ variable "root_folder" {
   type        = string
   description = "The folder to check for any changes"
 }
+
+variable "repository" {
+  type        = string
+  description = "repo setting for all environments"
+}
+
+
+// example input
+// {dev0={name=\"dev\",description=\"this is my dev project\",credential=\"aws dev\"}}
+variable "environments" {
+  type = map(
+    object({
+      project_id  = string
+      path        = string
+      //force_destroy = bool
+    })
+  )
+}
