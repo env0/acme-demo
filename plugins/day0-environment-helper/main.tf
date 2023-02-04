fb5aeaf8c688f6bd6530e380714022c922d9c3df
Andrew Way <away@env0.com>
2023-02-04 12:55:25 -0800

    let's go

--- plugins/day0-environment-helper/main.tf
+++ plugins/day0-environment-helper/main.tf
@@ -1,12 +1,20 @@
+data "env0_project" "project" {
+  for_each = local.environments
+  name     = each.value.project_name
+}
+
 resource "env0_environment" "dynamic_environment" {
   for_each   = local.environments
   name       = each.key
-  project_id = each.value.project_id
+  project_id = data.env0_project.project[each.key].id
+  workspace  = "dynamic-environment-${each.key}"
+
   //force_destroy = each.value.force_destroy
   force_destroy                    = true
   auto_deploy_on_path_changes_only = true
   deploy_on_push                   = true
   run_plan_on_pull_requests        = true
+  is_remote_backend                = true
 
   without_template_settings {
     repository             = var.repository
@@ -18,17 +26,22 @@ resource "env0_environment" "dynamic_environment" {
 }
 
 locals {
+  project_id   = "ccba5035-0b8d-4989-8a7a-f20b93801074"
+  revision     = "plugin-create-env"
+  project_name = "dynamic-environments"
   environments = {
     dev-svc = {
-      project_id        = "7ded3097-2e6d-47af-81f3-00ee6b841e6d"
+      project_id        = local.project_id
+      project_name      = local.project_name
       path              = "dynamic-environments/dev-svc"
-      revision          = "plugin-create-env"
+      revision          = local.revision
       is_remote_backend = true
     }
     dev-svc2 = {
-      project_id        = "7ded3097-2e6d-47af-81f3-00ee6b841e6d"
+      project_id        = local.project_id
+      project_name      = local.project_name
       path              = "dynamic-environments/dev-svc2"
-      revision          = "plugin-create-env"
+      revision          = local.revision
       is_remote_backend = true
     }
   }
