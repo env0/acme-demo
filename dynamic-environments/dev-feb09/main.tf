1f04a657d1589e208738c44a60decc4edac8d02f
Andrew Way <away@env0.com>
2023-02-09 14:38:43 -0800

    updae varialbe and length

--- dynamic-environments/dev-feb09/main.tf
+++ dynamic-environments/dev-feb09/main.tf
@@ -16,7 +16,8 @@ module "random" {
   source  = "api.env0.com/bde19c6d-d0dc-4b11-a951-8f43fe49db92/random/acme"
   version = "1.1.0"
 
-  refresh_date = var.refresh_date
+  refresh_token = var.refresh_date
+  length        = 10
 }
 
 output "random_string" {
