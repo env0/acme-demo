c3a433aeebbe1340d7608729b1fa0455a9e2d4d6
Andrew Way <away@env0.com>
2023-02-23 22:03:28 -0800

    almost done

--- simple-ec2-instance/importVariables.sh
+++ simple-ec2-instance/importVariables.sh
@@ -82,7 +82,7 @@ for ((i = 0; i < LENGTH; i++)); do
     fi
 
     # fetch value from environment 
-    SOURCE_OUTPUT_VALUE=$(jq -rc".[] | select(.name==\"$SOURCE_OUTPUT_NAME\") | .value" $SOURCE_ENV0_ENVIRONMENT_ID.env.json)
+    SOURCE_OUTPUT_VALUE=$(jq -r ".[] | select(.name==\"$SOURCE_OUTPUT_NAME\") | .value" $SOURCE_ENV0_ENVIRONMENT_ID.env.json)
     echo "${KEYS[i]}=$SOURCE_OUTPUT_VALUE"
     # unset ${KEYS[i]}
     echo "${KEYS[i]}=$SOURCE_OUTPUT_VALUE" >> $ENV0_ENV
