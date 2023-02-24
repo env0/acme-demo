779382f2caeb47809c27a0eff7eaf34ca0cf5c5f
Andrew Way <away@env0.com>
2023-02-23 21:29:48 -0800

    test new version of script

---
+++ simple-ec2-instance/importVariables.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+########################
+# Fetch Variables 
+# env0 Plugin to Pull outputs from an existing environment and save them as input variables
+#
+########################
+
+# Parse all tf vars from env0.auto.tfvars.json
+# look for each key, check value in format of ${env0:envid:outputname}
+# fetch the output variable using API and match outputname, then write key value with a new file env1.auto.tfvars.json
+
+# https://developer.hashicorp.com/terraform/language/values/variables#variable-definition-precedence
+# lexical order, so last file wins!
+
+KEYS=($(jq -rc 'keys | .[]' env0.auto.tfvars.json))
+VALUES=($(jq -c '.[]' env0.auto.tfvars.json))
+LENGTH=$(jq 'length' env0.auto.tfvars.json)
+
+TFVAR_FILENAME=env1.auto.tfvars
+if [[ -e $TFVAR_FILENAME ]]; then
+  rm $TFVAR_FILENAME
+else 
+  touch $TFVAR_FILENAME
+fi 
+
+# for each variable in env0.auto.tfvars.json 
+for ((i = 0; i < LENGTH; i++)); do
+  if [[ ${VALUES[i]} =~ ^(\"\$\{env0:) ]]; then
+    echo ${KEYS[i]}:${VALUES[i]}
+    # split the string across ':'
+    SPLIT_VALUES=($(echo ${VALUES[i]} | tr ":" "\n"))
+    SOURCE_ENV0_ENVIRONMENT_ID=${SPLIT_VALUES[1]}
+    len=$((${#SPLIT_VALUES[2]}-2))
+    SOURCE_OUTPUT_NAME=${SPLIT_VALUES[2]:0:$len}
+    echo "fetch value for ${KEYS[i]}:$SOURCE_OUTPUT_NAME from ${SOURCE_ENV0_ENVIRONMENT_ID}"
+
+    # fetch logs from environment
+    # TODO: Make it more efficient and not make a second call if the file already exists
+    curl -s --request GET \
+     --url https://api.env0.com/environments/$SOURCE_ENV0_ENVIRONMENT_ID \
+     --header 'accept: application/json' \
+     -u $ENV0_API_KEY:$ENV0_API_SECRET \
+     -o $SOURCE_ENV0_ENVIRONMENT_ID.json
+
+    # fetch value from environment 
+    SOURCE_OUTPUT_VALUE=$(jq ".latestDeploymentLog.output.$SOURCE_OUTPUT_NAME.value" $SOURCE_ENV0_ENVIRONMENT_ID.json)
+    #echo $SOURCE_OUTPUT_VALUE
+    
+    # store value in .auto.tfvars
+    echo "${KEYS[i]}=$SOURCE_OUTPUT_VALUE" >> $TFVAR_FILENAME
+  fi
+done
+
+# show updated values
+cat $TFVAR_FILENAME
+
+### Repeat process for Environment Variables
+KEYS=($(jq -rc 'keys | .[]' env0.env-vars.json))
+VALUES=($(jq -c '.[]' env0.env-vars.json))
+LENGTH=$(jq 'length' env0.env-vars.json)
+
+echo $LENGTH
+# write to ENV0_ENV
+# for each variable in env0.env-vars.json 
+for ((i = 0; i < LENGTH; i++)); do
+  if [[ ${VALUES[i]} =~ ^(\"\$\{env0:) ]]; then
+    echo ${KEYS[i]}:${VALUES[i]}
+    # split the string across ':'
+    SPLIT_VALUES=($(echo ${VALUES[i]} | tr ":" "\n"))
+    SOURCE_ENV0_ENVIRONMENT_ID=${SPLIT_VALUES[1]}
+    len=$((${#SPLIT_VALUES[2]}-2))
+    SOURCE_OUTPUT_NAME=${SPLIT_VALUES[2]:0:$len}
+    echo "fetch value for ${KEYS[i]}:$SOURCE_OUTPUT_NAME from ${SOURCE_ENV0_ENVIRONMENT_ID}"
+
+    # fetch logs from environment
+    if [[ ! -e $SOURCE_ENV0_ENVIRONMENT_ID.env.json ]]; then
+      curl --request GET \
+        --url "https://api.env0.com/configuration?organizationId=$ENV0_ORGANIZATION_ID&environmentId=$SOURCE_ENV0_ENVIRONMENT_ID" \
+        --header 'accept: application/json' \
+        -u $ENV0_API_KEY:$ENV0_API_SECRET \
+        -o $SOURCE_ENV0_ENVIRONMENT_ID.env.json
+    fi
+
+    # fetch value from environment 
+    SOURCE_OUTPUT_VALUE=$(jq ".[] | select(.name==\"$SOURCE_OUTPUT_NAME\") | .value" $SOURCE_ENV0_ENVIRONMENT_ID.env.json)
+    echo "${KEYS[i]}=$SOURCE_OUTPUT_VALUE" >> $ENV0_ENV
+  fi
+done
+
+cat $ENV0_ENV
