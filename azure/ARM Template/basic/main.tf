4e6af7bc57fb1edd955e0d33c24638e49b04b706
Andrew Way <away@env0.com>
2023-05-25 15:55:17 -0700

    remove null resource

--- azure/ARM Template/basic/main.tf
+++ azure/ARM Template/basic/main.tf
@@ -1,9 +1,8 @@
-resource "null_resource" "arm_plan" {
-  triggers = {
-    base64_arm_plan = base64encode(file("azuredeploy.plan.json"))
-  }
-}
-
+# resource "null_resource" "arm_plan" {
+#   triggers = {
+#     base64_arm_plan = base64encode(file("azuredeploy.plan.json"))
+#   }
+# }
 output "arm_plan" {
-  value = file("azuredeploy.plan.json")
+  value = file("azuredeploy.plan.json"se)
 }
