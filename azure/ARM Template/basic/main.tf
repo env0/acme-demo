fad832103242c72c1466b165bd65d4e4baef0784
Andrew Way <away@env0.com>
2023-05-25 15:34:31 -0700

    try null_resource

--- azure/ARM Template/basic/main.tf
+++ azure/ARM Template/basic/main.tf
@@ -2,6 +2,12 @@ locals {
   arm_plan = file(azuredeploy.plan.json)
 }
 
+resource "null_resource" "arm_plan" {
+  triggers = {
+    base64_arm_plan = base64encode(arm_plan)
+  }
+}
+
 output "arm_plan" {
   value = local.arm_plan
 }
