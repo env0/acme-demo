b0c63c17a429b20c8b6ebcad6729e22286eced19
Andrew Way <andrew.way@env0.com>
2023-06-02 12:05:08 -0700

    Update main.tf
    remove data provider and configuration variable

--- complex-deployment/infra-mid-1/main.tf
+++ complex-deployment/infra-mid-1/main.tf
@@ -32,24 +32,6 @@ module "infra" {
   refresh_token = "${var.deployment_key}_${var.env_name}"
 }
 
-resource "env0_configuration_variable" "infra_mid" {
-  name         = "${var.deployment_key}_${var.env_name}"
-  project_id   = var.project_id
-  value        = "${var.env_name}_${module.infra.random_string}"
-  is_read_only = true
-  type         = "terraform"
-}
-
-data "env0_configuration_variable" "infra_base" {
-  project_id = var.project_id
-  name       = "${var.deployment_key}_infra_base"
-}
-
-output "depends_on" {
-  value     = data.env0_configuration_variable.infra_base.value
-  sensitive = true
-}
-
 output "infra_name" {
   value = "${var.env_name}_${module.infra.random_string}"
 }
