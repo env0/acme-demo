d52c2123e1a938d8b4186052f174800f975dbc49
Andrew Way <andrew.way@env0.com>
2023-06-27 23:18:26 -0700

    Update main.tf

--- workflows/a-network/main.tf
+++ workflows/a-network/main.tf
@@ -1,15 +1,3 @@
-
-
-variable "length" {
-  type    = number
-  default = 5
-}
-
-variable "refresh_token" {
-  type    = string
-  default = "0"
-}
-
 module "vpc" {
   source  = "api.env0.com/bde19c6d-d0dc-4b11-a951-8f43fe49db92/random/env0"
   version = "~> 2.0"
