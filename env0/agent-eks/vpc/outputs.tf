849de29e3d3bca13010c06a617041a71aa156446
Andrew Way <away@env0.com>
2023-09-08 14:17:02 -0700

    vpc update

--- env0/agent-eks/vpc/outputs.tf
+++ env0/agent-eks/vpc/outputs.tf
@@ -1,5 +1,6 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  value       = module.vpc.vpc_id
+  description = "VPC id"
 }
 
 output "private_subnets" {
--- env0/agent-eks/vpc/provider.tf
+++ env0/agent-eks/vpc/provider.tf
@@ -2,16 +2,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>3.68.0"
+      version = "~>5.0"
     }
   }
 
   backend "remote" {
     hostname     = "backend.api.env0.com"
-    organization = "bde19c6d-d0dc-4b11-a951-8f43fe49db92"
+    organization = "bde19c6d-d0dc-4b11-a951-8f43fe49db92.1a433171-217e-4f58-9b4e-308d4d77902f"
 
     workspaces {
-      prefix = "acme-fitness-agent-"
+      prefix = "acme-cluster-"
     }
   }
 }
