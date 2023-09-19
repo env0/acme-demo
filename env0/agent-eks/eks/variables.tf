a7c1b134d43f23e9d94b700091f3f21741a7f057
Andrew Way <away@env0.com>
2023-09-19 16:31:25 -0700

    fix arn for kms admins

--- env0/agent-eks/eks/variables.tf
+++ env0/agent-eks/eks/variables.tf
@@ -38,7 +38,7 @@ variable "map_roles" {
 variable "kms_key_administrators" {
   type = list(string)
   default = ["arn:aws:iam::326535729404:role/env0-acme-assume-role",
-  "arn:aws:sts::326535729404:assumed-role/AWSReservedSSO_AdministratorAccess_6e013e7aceaa4447"]
+  "arn:aws:iam::326535729404:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AdministratorAccess_6e013e7aceaa4447"]
 }
 
 variable "min_capacity" {
