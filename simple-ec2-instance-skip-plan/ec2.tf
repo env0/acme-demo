70e4eac100b5834984cbe2a3e70a72b4d5ec8a80
Asaf Blubshtein <57428051+asafsb@users.noreply.github.com>
2023-12-21 23:08:46 -0600

    Update ec2.tf

--- simple-ec2-instance-skip-plan/ec2.tf
+++ simple-ec2-instance-skip-plan/ec2.tf
@@ -15,7 +15,7 @@ module "acme-ec2" {
     Terraform = "true"
     Owner     = "acme demo org"
     Test      = "Change with script"
-    Demo      = "Dec 21"
+    Demo      = "Dec 21 2023"
   }
 }
 
