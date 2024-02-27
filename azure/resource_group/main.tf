ab2a858c8557aa4c6a64a71e12149cf04751ef33
Andrew Way <away@env0.com>
2024-02-27 13:05:27 -0800

    add features and provider

--- azure/resource_group/main.tf
+++ azure/resource_group/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">=3.93.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+  }
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-sales-rg"
   location = var.location
