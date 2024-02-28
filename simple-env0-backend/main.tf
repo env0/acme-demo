fd67148823193b691a2b5366b7cfd6e9f69df95c
Asaf Blubshtein <57428051+asafsb@users.noreply.github.com>
2024-02-28 13:13:21 -0600

    Update main.tf

--- simple-env0-backend/main.tf
+++ simple-env0-backend/main.tf
@@ -10,7 +10,7 @@ terraform {
     hostname = "backend.api.env0.com"
     organization = "bde19c6d-d0dc-4b11-a951-8f43fe49db92.edba559d-ae7f-467d-a2f8-1b5a54709ead"
     workspaces {
-      name = "test-remote-backend-20240102"
+      name = "test-remote-backend-20240228"
     }
   }
 }
