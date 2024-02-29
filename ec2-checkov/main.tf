58e9ac9845afb49f3e8821fad305751e2ce64775
Andrew Way <away@env0.com>
2024-02-29 13:10:55 -0800

    update checkov instance

--- ec2-checkov/main.tf
+++ ec2-checkov/main.tf
@@ -1,25 +1,16 @@
-# module "acme-ec2" {
-#   source = "api.env0.com/bde19c6d-d0dc-4b11-a951-8f43fe49db92/ec2-instance/env0"
-#   version = "~> 1.0.1"
+module "acme-ec2" {
+  source = "api.env0.com/bde19c6d-d0dc-4b11-a951-8f43fe49db92/ec2-instance/env0"
+  version = "~> 1.0.1"
 
-#   name           = "ec2-checkov-demo-instance"
+  name           = "ec2-checkov-demo-instance"
+  instance_type  = "t3a.nano"
 
-#   associate_public_ip_address = true
-
-#   user_data = <<EOF
-# #!/bin/bash
-# sudo apt-get -y update
-# sudo apt-get -y install nginx
-# export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
-# sudo echo "Welcome to env0, this is: $PUBLIC_IPV4" > /usr/share/nginx/html/index.html
-# EOF
-
-#   tags = {
-#     Terraform   = "true"
-#     Environment = "dev"
-#     Owner       = "env0 Acme Demo"
-#   }
-# }
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    Owner       = "env0 Acme Demo"
+  }
+}
 
 module "ssh_sg" {
   source = "terraform-aws-modules/security-group/aws//modules/ssh"
