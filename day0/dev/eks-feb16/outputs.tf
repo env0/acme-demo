2c16604cb10be6fd1f4e26f8d6a80980706a1933
Andrew Way <away@env0.com>
2024-03-13 21:16:41 -0700

    reset environment discovery and day0

--- day0/dev/eks-feb16/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "id" {
-  value = resource.random_string.random.id
-}
-
-output "result" {
-  value = resource.random_string.random.result
-}
---
+++ environment-discovery/template/ec2/main.tf
@@ -0,0 +1,14 @@
+module "acme-ec2" {
+  source = "api.env0.com/bde19c6d-d0dc-4b11-a951-8f43fe49db92/ec2-instance/env0"
+  version = "~> 1.0.0"
+
+  name           = "ec2-checkov-demo-instance"
+  instance_type  = "t3a.nano"
+  vpc_id = var.vpc_id
+  
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    Owner       = "env0 Acme Demo"
+  }
+}
