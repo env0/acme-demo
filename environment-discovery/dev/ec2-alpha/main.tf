7c7a13d1c7427fcca40809150cbcd8870a8695c6
Andrew Way <away@env0.com>
2024-03-13 21:37:24 -0700

    request for new ec2 instance

--- environment-discovery/dev/ec2-alpha/main.tf
+++ environment-discovery/dev/ec2-alpha/main.tf
@@ -2,7 +2,7 @@ module "acme-ec2" {
   source = "api.env0.com/bde19c6d-d0dc-4b11-a951-8f43fe49db92/ec2-instance/env0"
   version = "~> 1.0.0"
 
-  name           = "ec2-demo-instance"
+  name           = "ec2-alpha"
   instance_type  = "t3a.nano"
   vpc_id = var.vpc_id
   
---
+++ environment-discovery/dev/ec2-alpha/outputs.tf
@@ -0,0 +1,7 @@
+output "public_ip" {
+  value = module.acme-ec2.public_ip
+}
+
+output "instances" {
+  value = module.acme-ec2.id
+}
