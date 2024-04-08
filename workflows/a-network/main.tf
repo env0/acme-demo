827a581cb7a1f354f08cc4e7a5f0ce1a6a30c3f8
Andrew Way <away@env0.com>
2024-04-08 08:50:02 -0700

    update module

--- workflows/a-network/main.tf
+++ workflows/a-network/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-  source  = "api.env0.com/bde19c6d-d0dc-4b11-a951-8f43fe49db92/random/env0"
-  version = "~> 2.0"
+  source  = "api.env0.com/bde19c6d-d0dc-4b11-a951-8f43fe49db92/random/acme"
+  version = "~> 1.0"
 
   length        = var.length
   refresh_token = var.refresh_token
