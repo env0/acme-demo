231d973d364f044e841c71340cddc8d04f91a288
Konstantin Zelmanovich <konstantin.zelmanovich@env0.com>
2024-04-16 17:42:35 +0300

    Update ec2.tf

--- simple-ec2-instance/ec2.tf
+++ simple-ec2-instance/ec2.tf
@@ -15,7 +15,7 @@ module "acme-ec2" {
     Terraform = "true"
     Owner     = "acme demo org"
     Test      = "new tag"
-    Demo      = "April 15 - 4:30PM ET"
+    Demo      = "April 16"
   }
 }
 
