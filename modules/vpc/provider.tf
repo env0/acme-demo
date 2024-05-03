b05d4ff9e36cd32dfc81447c25351d86d0dd6e89
Andrew Way <andrew.way@env0.com>
2024-05-03 13:28:43 -0700

    fix provider version

--- modules/vpc/provider.tf
+++ modules/vpc/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.41.0"
+      version = ">= 5.0"
     }
     env0 = {
       source = "env0/env0"
-      version = ">= 1.0.2"
+      version = ">= 1.0"
     }
   }
 }
