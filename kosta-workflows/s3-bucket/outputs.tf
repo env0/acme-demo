7264ec9af75d152edc7108b5088354f27e839400
Konstantin Zelmanovich <Constantine19>
2024-05-05 17:42:08 +0300

    remove comments

--- kosta-workflows/s3-bucket/outputs.tf
+++ kosta-workflows/s3-bucket/outputs.tf
@@ -1,8 +1,3 @@
-# output "bucket_name" {
-# #   value = "kosta-bucket-${module.random.result}"
-#   value = aws_s3_bucket.foo-bucket.bucket
-# }
-
 output "bucket_name" {
   description = "The name of the S3 bucket"
   value       = aws_s3_bucket.kosta-bucket.bucket
