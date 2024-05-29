2e5e31fb0444dc38c35283e19f1ad422d605d5e7
Konstantin Zelmanovich <Constantine19>
2024-05-29 14:34:53 +0300

    comment block

--- testing-workflows/randomize-name/variables.tf
+++ testing-workflows/randomize-name/variables.tf
@@ -13,29 +13,29 @@ variable "numeric" {
     default = false
 }
 
-sub_environment_configuration {
-  alias = "workload-kubernetes-addon-argo-rollouts"
-  revision = var.revision
+# sub_environment_configuration {
+#   alias = "workload-kubernetes-addon-argo-rollouts"
+#   revision = var.revision
 
-  configuration {
-    type = "environment"
-    name = "ENVO_TERRAFORM_CONFIG_FILE_PATH"
-    value = "../../../../../live/${var.environment}/${each.value.name}/platform/workload/kubernetes/addons/argo-rollouts/terraform.tfvars"
-    is_required = true
-  }
+#   configuration {
+#     type = "environment"
+#     name = "ENVO_TERRAFORM_CONFIG_FILE_PATH"
+#     value = "../../../../../live/${var.environment}/${each.value.name}/platform/workload/kubernetes/addons/argo-rollouts/terraform.tfvars"
+#     is_required = true
+#   }
 
-  configuration {
-    type = "terraform"
-    name = "oidc_provider_arn"
-    value = "$${env0-workflow: workload-kubernetes-cluster:oidc_provider_arn}"
-    is_required = true
-  }
+#   configuration {
+#     type = "terraform"
+#     name = "oidc_provider_arn"
+#     value = "$${env0-workflow: workload-kubernetes-cluster:oidc_provider_arn}"
+#     is_required = true
+#   }
 
-  configuration {
-    type = "terraform"
-    name = "cluster_name"
-    value = "$${env0-workflow: workload-kubernetes-cluster: cluster_name}"
-    is_required = true
-  }
+#   configuration {
+#     type = "terraform"
+#     name = "cluster_name"
+#     value = "$${env0-workflow: workload-kubernetes-cluster: cluster_name}"
+#     is_required = true
+#   }
 
-}
+# }
