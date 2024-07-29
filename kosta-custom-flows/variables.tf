62b2e4803bbd0da2976555aea495ffc1fbfd092c
Konstantin Zelmanovich <konstantin.zelmanovich@env0.com>
2024-07-29 16:18:02 +0300

    Update variables.tf

--- kosta-custom-flows/variables.tf
+++ kosta-custom-flows/variables.tf
@@ -6,3 +6,23 @@ variable "ssh_cidr_block" {
 variable "vpc_id" {
   type = string
 }
+
+# Example of a List
+variable "menu_item" {
+  type        = list(string)
+  description = "A list of the items to order from the menu"
+  default     = ["philly", "medium", "thin"]
+}
+
+# Example of a Map
+variable "credit_card" {
+  type        = map(string)
+  description = "Credit Card Info"
+  sensitive   = true
+  default     = {
+    number      = 123456789101112
+    cvv         = 1314
+    date        = "15/16"
+    postal_code = "18192"
+  }
+}
