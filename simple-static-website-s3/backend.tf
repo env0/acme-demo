31e41d5eee8dedb581deec16cb3336b00ffab588
Andrew Way <andrew.way@env0.com>
2024-10-21 12:33:20 -0700

    add custom flow

---
+++ simple-static-website-s3/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  cloud {
+    hostname     = "backend.api.env0.com"
+    organization = "bde19c6d-d0dc-4b11-a951-8f43fe49db92"
+    workspaces {
+      name = "static-website-dev1"
+    }
+  }
+}
