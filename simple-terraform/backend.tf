1fbf6949ad82078093e86852b831251a6480be10
Andrew Way <andrew.way@env0.com>
2024-11-06 15:58:49 -0800

    Update backend.tf

--- simple-terraform/backend.tf
+++ simple-terraform/backend.tf
@@ -1,12 +1,12 @@
 terraform {
-  required_version = "= 1.8.3"
+#  required_version = "> 1.8.3"
 
-  cloud {
-    hostname     = "backend.api.env0.com"
-    organization = "bde19c6d-d0dc-4b11-a951-8f43fe49db92"
+#  cloud {
+#    hostname     = "backend.api.env0.com"
+#    organization = "bde19c6d-d0dc-4b11-a951-8f43fe49db92"
 
-    workspaces {
-      name = "simple-terraform-64218540"
-    }
-  }
+#    workspaces {
+#      name = "simple-terraform-64218540"
+#    }
+#  }
 }
