0b41e8a73fbbde4480efe543f29af37bfd818639
Konstantin Zelmanovich <Constantine19>
2025-01-23 07:33:41 -0500

    add files for webinar

---
+++ webinar/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.75.0"
+    }
+  }
+}
+
+variable "region" {
+  type    = string
+  default = "us-west-2"
+}
+
+variable "bucketname" {
+  type    = string
+  default = "env0-acme-bucket"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "random_string" "random" {
+  length  = 6
+  special = false
+  lower   = true
+  upper   = false
+  number  = true
+}
+
+module "s3" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.0.0"
+
+  bucket = "${var.bucketname}-${random_string.random.id}"
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerPreferred"
+}
+  
+output "s3_bucket_arn" {
+  value = module.s3.s3_bucket_arn
+}
