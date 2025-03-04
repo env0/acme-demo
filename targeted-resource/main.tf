a2257af5a446c4ed2f89709a772b6388f9f7bfea
yarivg <yarivgavriel2@gmail.com>
2025-03-04 07:20:18 -0600

    add targeted resource

---
+++ targeted-resource/main.tf
@@ -0,0 +1,47 @@
+# main.tf (IAM Roles Module)
+resource "aws_iam_role" "roles" {
+  for_each = {
+    karpenter-node      = "Karpenter Node Role"
+    karpenter-controller = "Karpenter Controller Role"
+  }
+
+  name               = each.key
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_policy" "custom" {
+  for_each = {
+    karpenter-node      = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+    karpenter-controller = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  }
+
+  name   = "${each.key}-policy"
+  policy = data.aws_iam_policy_document.inline.json
+}
+
+resource "aws_iam_role_policy_attachment" "custom" {
+  for_each = {
+    karpenter-node      = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+    karpenter-controller = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  }
+
+  role       = aws_iam_role.roles[each.key].name
+  policy_arn = each.value
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "inline" {
+  statement {
+    actions   = ["s3:ListBucket", "ec2:DescribeInstances"]
+    resources = ["*"]
+  }
+}
