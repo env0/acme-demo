85f396199e489892e5deddeaf8025183fc0726be
Hever Farber <hever.farber@gmail.com>
2025-11-03 15:16:52 +0200

    bundle

--- env0/custom-image/spectral-image/build-and-run.sh
+++ env0/custom-image/spectral-image/build-and-run.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-docker build --tag visa-image --progress plain -f Dockerfile .
-docker run --rm -it --env-file "${ENV_FILE}" visa-image
+export USER_ID=2001210001
+
+docker build --build-arg USER_ID=${USER_ID} --tag visa-image --progress plain -f Dockerfile .
+docker build --tag visa-image-bundle --progress plain -f bundle.Dockerfile .
+
+docker run --rm -it --read-only \
+             -e STRICT_SECURITY_CONTEXT="true" \
+            --tmpfs /tmp:uid=${USER_ID},gid=1000 --tmpfs /home/node:uid=${USER_ID},gid=1000 --tmpfs /opt/packages:uid=${USER_ID},gid=1000,exec  \
+            --user ${USER_ID} --env-file "${ENV_FILE}" visa-image-bundle
